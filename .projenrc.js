5808c7c9b4ac807859d9aa38638f059948bd9aef
sakthi s <velsakthis5301@gmail.com>
2022-05-16 15:44:38 +0530

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require("projen");
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: "sakthi s",
+  authorAddress: "velsakthis5301@gmail.com",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "secure-bucket",
+  repositoryUrl: "https://github.com/velsakthis5301/secure-bucket.git",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
